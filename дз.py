f82201d9c4f7106d135138c2720f4dba97c29e6a
eugene <156702784+XOTTKNHC@users.noreply.github.com>
2024-09-26 23:29:59 +0300

    Add files via upload

--- дз.py
+++ дз.py
@@ -1,32 +1,36 @@
-import math
-#r=int(input("Введите значение r: "))
-#print("Длина кружности равна",round(2*math.pi*r,2),"\nПлощадь круга равна",round(math.pi*(r**2),2))
+while True:
+    try:
+        a=float(input("Введите первое число a="))
+        b=float(input("Введите второе число b="))
+        c=a/b
+        break
+    except ValueError:
+        print("Ошибка, это не число")
+    except ZeroDivisionError:
+        print("Ошибка, деление на ноль")
+print(a,"/",b,"=",c)
 
-#x=10
-#y=55
-#print(x,y)
-#x,y=y,x
-#print(x,y)
-
-#g=9.81
-#l=float(input("Введите значение l"))
-#print(round(2*math.pi*((l/g)**0.5),2))
-
-#n1=float(input("n1="))
-#n2=float(input("n2="))
-#if n2==0:
-#    print("Ошибка, деление на ноль")
-#else:
-#    print("n1/n2=",n1/n2)
-
-#s=float(input("Стоимость покупки= "))
-#if s>20:
-#    s=s*0.65
-#print("Итоговая стоимость:",round(s,2))
+while True:
+    try:
+        s=float(input("Введите сумму покупки: "))
+        if s!=abs(s):
+            print("Ошибка, отрицательное число")
+            continue
+        break
+    except:
+        print("Ошибка, это не число")
+if s>20:
+    s=s*0.65
+print("Стоимость покупки =",s)
 
 a={1:"Январь",2:"Февраль",3:"Март",4:"Апрель",5:"Май",6:"Июнь",7:"Июль",8:"Август",9:"Сентябрь",10:"Октябрь",11:"Ноябрь",12:"Декабрь"}
-g=int(input("Номер месяца: "))
-if g in a:
-    print(a[g])
-else:
-    print("Ошибка")
+while True:
+    try:
+        g=int(input("Введите номер месяца: "))
+        if g>12 or g<1:
+            print("Ошибка, месяца с таким номером не существует")
+            continue
+        break
+    except:
+        print("Ошибка, это не число")
+print("Это",a[g])
